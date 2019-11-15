cfdf630f25ff3290373f57587a42172693bc6ecd
WuShaoling <wsl6@outlook.com>
2019-11-15 22:29:01 +0800

    refactor: enable merge array when load config
    Signed-off-by: WuShaoling <wsl6@outlook.com>

--- app/plugin/config-manager/AppConfigLoader.ts
+++ app/plugin/config-manager/AppConfigLoader.ts
@@ -14,9 +14,8 @@
 
 import { AppConfigManagerConfig } from './AppConfigManager';
 import { Application } from 'egg';
-import { parseRepoName } from '../../basic/Utils';
+import { parseRepoName, customizerMerge } from '../../basic/Utils';
 import { readFileSync, existsSync } from 'fs';
-import extend from 'extend2';
 import { join } from 'path';
 
 export class AppConfigLoader {
@@ -33,13 +32,20 @@ export class AppConfigLoader {
     const mysqlConfig = await this.loadConfigFromMysql(managerConfig);
     const fileConfig = await this.loadConfigFromFile(managerConfig, installationId, fullName);
     const remoteConfig = await this.loadConfigFromRemote(managerConfig, installationId, fullName);
-    const mergeConfig = extend(true, { }, this.app.config, fileConfig, mysqlConfig, remoteConfig);
+    const defaultConfig = await this.loadDefaultConfig(managerConfig);
+
+    const mergeConfig = customizerMerge(defaultConfig, fileConfig, mysqlConfig, remoteConfig);
+    if (mergeConfig.error && mergeConfig.error.length !== 0) {
+      mergeConfig.error.forEach(err => {
+        this.app.logger.error(err);
+      });
+    }
 
     // filter
     const retConfig: any = { };
     managerConfig.configurable.forEach(k => {
-      if (mergeConfig[k]) {
-        retConfig[k] = mergeConfig[k];
+      if (mergeConfig.config[k]) {
+        retConfig[k] = mergeConfig.config[k];
       }
     });
 
@@ -86,6 +92,16 @@ export class AppConfigLoader {
       this.app.logger.error(error);
     }
     return { };
+  }
+
+  private async loadDefaultConfig(managerConfig: AppConfigManagerConfig): Promise<any> {
+    const defaultConfig: any = { };
+    managerConfig.configurable.forEach(k => {
+      if (this.app.config[k]) {
+        defaultConfig[k] = this.app.config[k];
+      }
+    });
+    return defaultConfig;
   }
 
   // rootPath_number_owner_repoName.json <= (installationId, fullName)
