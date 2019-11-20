e7a781459fedfb2b8d49570e85432451e84a19dc
WuShaoling <wsl6@outlook.com>
2019-11-20 22:38:25 +0800

    feature: add GitHub data collector mentioned in #11

--- app/plugin/github-installation-manager/github-client/GitHubClient.ts
+++ app/plugin/github-installation-manager/github-client/GitHubClient.ts
@@ -14,7 +14,7 @@
 
 import Octokit = require('@octokit/rest');
 import { IClient } from '../../installation-manager/IClient';
-import { parseRepoName, BotLogger } from '../../../basic/Utils';
+import { parseRepoName, BotLogger, waitUntil, ParseDate } from '../../../basic/Utils';
 import { GitHubInstallation } from '../github-installation/GitHubInstallation';
 import { Repo } from '../../../basic/DataTypes';
 
@@ -40,6 +40,75 @@ export class GitHubClient implements IClient {
 
   private async updateData(): Promise<void> {
     this.logger.info(`Start to update data for ${this.name} in ${this.installation.id}`);
+
+    const dataCat = this.installation.dataCat;
+    await waitUntil(() => dataCat.inited);
+
+    const full = await dataCat.repo.full(this.owner, this.repo, {
+        contributors: true,
+        issues: true,
+        stars: true,
+        forks: true,
+        pulls: true,
+      });
+
+    this.repoData = {
+      ...full,
+      stars: full.stars.map(star => {
+        return {
+          ...star,
+          time: new Date(star.time),
+        };
+      }),
+      forks: full.forks.map(fork => {
+        return {
+          ...fork,
+          time: new Date(fork.time),
+        };
+      }),
+      issues: full.issues.map(issue => {
+        return {
+          ...issue,
+          createdAt: new Date(issue.createdAt),
+          updatedAt: new Date(issue.updatedAt),
+          closedAt: ParseDate(issue.closedAt),
+          comments: issue.comments.map(comment => {
+            return {
+              ...comment,
+              createdAt: new Date(comment.createdAt),
+            };
+          }),
+        };
+      }),
+      pulls: full.pulls.map(pull => {
+        return {
+          ...pull,
+          createdAt: new Date(pull.createdAt),
+          updatedAt: new Date(pull.updatedAt),
+          closedAt: ParseDate(pull.closedAt),
+          mergedAt: ParseDate(pull.mergedAt),
+          comments: pull.comments.map(c => {
+            return {
+              ...c,
+              createdAt: new Date(c.createdAt),
+            };
+          }),
+          reviewComments: pull.reviewComments.map(rc => {
+            return {
+              ...rc,
+              createdAt: new Date(rc.createdAt),
+            };
+          }),
+        };
+      }),
+      contributors: full.contributors.map(c => {
+        return {
+          ...c,
+          time: new Date(c.time),
+        };
+      }),
+    };
+    console.log(JSON.stringify(this.repoData));
   }
 
   public async getFileContent(path: string): Promise<string | undefined > {
